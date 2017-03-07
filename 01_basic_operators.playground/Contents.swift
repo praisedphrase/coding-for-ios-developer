de5d0ee2f07e7dca2d39b3f6549f1792e9b08f0d
theronapei <seymouryoung@theronapeiui-MacBook-Pro.local>
2017-03-07 18:38:42 +0900

    add basic operators

---
+++ 01_basic_operators.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//str
+
+var number1 = 1
+number1 = 3
+number1 += 5
+
+
+let number2 = 2
+//number2 = 4
+
+// str = 5
+
+1 + 2
+5 - 3
+2 * 3
+10.0 / 2.5
+
+5 / 2
+5 % 2
+
+"abc" + "def"
+ 
